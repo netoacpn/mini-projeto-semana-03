d94ef0fa5db0bdc96450cdbfc5f371a766bf8d7f
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-07-09 11:12:04 -0300

    Bootstrap heart icon add

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ex1 - [header] Adicionar Informações do Header
+// Em JavaScript que selecione o elemento do header. Em seguida, adicione dinamicamente as informações do header
+// (Informações da logo e opções de menu)
+// Sugestão: Crie um objeto em Javascript com informações da logo e uma lista de opções de menu.
+
